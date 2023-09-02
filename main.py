94827f7bbc4cdf7b38f6501ec78839823853f8f0
Khai Do <khaitroyy@gmail.com>
2023-09-02 15:51:08 -0500

    add new open command

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("new_file.txt", mode="w") as file:
-    file.write("Hello, my name is Khai. ")
+# with open("new_file.txt", mode="w") as file:
+#     file.write("Hello, my name is Khai. ")
+
+with open("my_file.txt", mode="r") as file:
+    content = file.read()
+    print(content)
